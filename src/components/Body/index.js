01d6ab39f241be264894f1c886f7e3b992c8fe42
Likpalmo marius N'KOUBA <nkoubamarius@gmail.com>
2020-12-24 00:48:04 +0100

    Mapping data from Axios request

--- src/components/Body/index.js
+++ src/components/Body/index.js
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Repository from "../Repository";
 import { BodyContainer } from "./BodyElements";
+import axios from "axios";
 
 const Body = () => {
+  const [repositories, setRepositories] = useState([]);
+  const GITHUB_LINK =
+    "https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc";
+  useEffect(() => {
+    axios
+      .get(GITHUB_LINK)
+      .then(function (response) {
+        setRepositories(response.data.items);
+        console.log(response.data.items);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <BodyContainer>
-      <Repository />
-      <Repository />
-      <Repository />
+      {repositories.map((repository) => (
+        <Repository key={repository.node_id} data={repository} />
+      ))}
     </BodyContainer>
   );
 };
