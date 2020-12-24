31142497dcbde6c2f6536042a260d018024c26fc
Likpalmo marius N'KOUBA <nkoubamarius@gmail.com>
2020-12-24 11:04:57 +0100

    Implement Pagination

--- src/components/Body/index.js
+++ src/components/Body/index.js
@@ -2,26 +2,51 @@ import React, { useEffect, useState } from "react";
 import Repository from "../Repository";
 import { BodyContainer } from "./BodyElements";
 import axios from "axios";
+import Pagination from "../Pagination";
 
 const Body = () => {
   const [repositories, setRepositories] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [repositoriesPerPage] = useState(10);
   const GITHUB_LINK =
-    "https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc";
+    "https://api.github.com/search/repositories?q=created:>2020-11-24&sort=stars&order=desc";
   useEffect(() => {
     axios
       .get(GITHUB_LINK)
       .then(function (response) {
         setRepositories(response.data.items);
-        console.log(response.data.items);
+        setLoading(false);
       })
       .catch(function (error) {
         console.log(error);
       });
   }, []);
 
+  if (loading) {
+    return <h2>Loading ....</h2>;
+  }
+
+  //Get current posts
+  const indexOfLastRepository = currentPage * repositoriesPerPage;
+  const indexOfFirstRepository = indexOfLastRepository - repositoriesPerPage;
+  const currentRepositories = repositories.slice(
+    indexOfFirstRepository,
+    indexOfLastRepository
+  );
+
+  // Change page
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
   return (
     <BodyContainer>
-      {repositories.map((repository) => (
+      <Pagination
+        repositoriesPerPage={repositoriesPerPage}
+        totalRepositories={repositories.length}
+        paginate={paginate}
+      />
+
+      {currentRepositories.map((repository) => (
         <Repository key={repository.node_id} repository={repository} />
       ))}
     </BodyContainer>
